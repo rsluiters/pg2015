697c1968ce3487ea4f1156fd9c1f0b21f543a92b
Ralph Sluiters <ralph@sluiters.de>
2014-03-20 20:28:48 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pg2015::Application.config.secret_key_base = '964c079507dfd7e893979875a7b10ec09c8e561b71f5d1b24d09ba0f62e48509d488ec3abe648e14a98e0454db21fd362e0179aca21865b4d5f57bcbd09fed7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pg2015::Application.config.session_store :cookie_store, key: '_pg2015_session'
